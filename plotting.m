71634d71b65429ae1d9fab78ed10e5f522d1e438
Kevin Robert Stravers <macocio@gmail.com>
2017-03-20 20:35:44 +0100

    Save glorious work...

--- plotting.m
+++ plotting.m
@@ -1,41 +1,49 @@
-close all
+close all;
 % Define figure size
 width = 10; % cm
 height = 10; % cm
 fontsize = 10; % points
-x = 20; y = 20; % Where on the screen the plot will appear, not important.
-set(0,'DefaultTextInterpreter', 'latex') % Interpret (most) text as LaTeX.
+x = 20;
+y = 20; % Where on the screen the plot will appear, not important.
+set(0, 'DefaultTextInterpreter', 'latex') % Interpret (most) text as LaTeX.
 
 % % 4.1.a
-% load('log_4.1.a.mat')
+%load('log_4.1.a.mat')
 
 % %4.1.b
 % load('log_4.1.b.mat')
 
 % %4.2.a
-% load('log.mat')
+load('log.mat')
 
- 
-eta = ans(2:4,:); 
-eta_d = ans(5:7,:); 
-s = ans(8,:); 
-t = ans(1,:); 
+eta = ans(2:4,:);
+eta_d = ans(5:7,:);
+s = ans(8,:);
+t = ans(1,:);
 
-%%
-  % Path in xy-plane
-  fig = figure(1);
-   fig.Units = 'centimeters';   %?? -Helene
-   plot(eta_d(1,:),eta_d(2,:), 'ro'); 
-     axis([-5,10,-5,10])
-     legend('desired position or path')
-    
-    
+% Path in xy-plane
+fig = figure(1);
+fig.Units = 'centimeters';   %?? -Helene
+plot(eta_d(1,:),eta_d(2,:), 'ro');
+axis([-5,15,-5,15])
+legend('desired position or path')
+
+figure;
+plot(simout.time, simout.data(:,3)*180/pi);
+hold on;
+plot(simout.time, eta_d(3,:)*180/pi);
+plot(simout.time, eta(3,:)*180/pi);
+%%legend('d/dt psi_d', 'psi_d', 'psi');
+pause;
+
     h = animatedline;
 
 for k = 1:length(eta(1,:))
     addpoints(h,eta(1,k),eta(2,k));
-    drawnow
-    pause(0.1)
+    if mod(k, 1000) == 0
+        drawnow;
+        pause(0.01);
+    end
 end
 
 % Desired vs measured x-dir
@@ -48,6 +56,6 @@ title('Desired path x-direction')
 % Desired vs measured y-dir
 figure
 plot(t,eta_d(2,:),'b',t,eta(2,:),'r',t,ones(size(t))*4,'--')
-axis([0,500,0,50])
+axis([0,500,-inf,inf])
 legend('Desired path','measured position','desired end point')
 title('Desired path y-direction')
